8021010a49e5fede449a5ab4a1a92823ba839360
otis-ke <vincentotiskisia.com>
2022-12-08 16:23:35 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/*
+ * puts prints a new line
+ * int main is the main method 
+ * return(0) returns an integer 
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+		return(0);
+}
