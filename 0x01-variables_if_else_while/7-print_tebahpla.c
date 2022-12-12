1bbf68ced6bda8070f3c653bc0e360862d7f1a0d
otis-ke <vincentotiskisia.com>
2022-12-11 18:46:19 -0800

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'Two number combinations'
+ *
+ *Return: 0(Success)
+ */
+int main(void)
+{
+	char le;
+
+	for (le = 'z'; le >= 'a'; le--)
+		putchar(le);
+	putchar("\n");
+	return (0);
+}
