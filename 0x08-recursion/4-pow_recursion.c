c1ecaab07acbed989b6b268d1eadd26902d31e1a
otis-ke <110315228+otis-ke@users.noreply.github.com>
2023-01-05 17:24:49 +0300

    commited last

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,24 +1,23 @@
+
 #include "main.h"
 /**
- * factorial - factorial of int
- *  @n:int
- *  Return:int
+ * _pow_recursion - power
+ * @x:int
+ * @y:int
+ * Return:int
  */
-int factorial(int n)
-{
-	int x;
 
-	if (n == 0)
-	{
-		return (1);
-	}
-	else if (n < 0)
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
 	{
 		return (-1);
 	}
+	else if (y != 0)
+		return (x * _pow_recursion(x, y - 1));
+
 	else
 	{
-		x = n * factorial(n - 1);
+		return (1);
 	}
-	return (x);
 }
