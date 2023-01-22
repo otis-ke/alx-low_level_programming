f278a48b7d51a9ef244bed5b926bc6325bde53d2
otis-ke <vincentotiskisia.com>
2023-01-22 01:58:27 -0800

    probleme

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -4,19 +4,22 @@
  * @n: number of parameter
  * @separator: separator between two numbers
  */
-void print_numbers(const char *separator, const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...)
 {
+	char *str;
 	unsigned int i;
 	va_list ap;
 
 	va_start(ap, n);
-
 	if (separator == NULL)
 		separator = "";
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%d", va_arg(ap, int));
+		str = va_arg(ap, char*);
+		if (str == NULL)
+			str = "(nil)";
+		printf("%s", str);
 		if (i < n - 1)
 			printf("%s", separator);
 	}
